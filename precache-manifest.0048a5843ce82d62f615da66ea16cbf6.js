9ab61a9076684a89693b7b391e1843e86f818e93
rishabkhanna27 <rishabkhanna27@gmail.com>
2020-06-12 13:16:30 +0530

    Updates

---
+++ precache-manifest.0048a5843ce82d62f615da66ea16cbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e5f294ff47ba977ec989fc895aa8d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2894268dbaa851a5640",
+    "url": "/static/css/main.aa962a6e.chunk.css"
+  },
+  {
+    "revision": "9345f4aedc92db6db292",
+    "url": "/static/js/2.cd25df68.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.cd25df68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2894268dbaa851a5640",
+    "url": "/static/js/main.d00f3ac8.chunk.js"
+  },
+  {
+    "revision": "99c4c7f6709bf2bb35fa",
+    "url": "/static/js/runtime-main.f231aec5.js"
+  }
+]);
